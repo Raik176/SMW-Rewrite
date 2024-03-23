ffba77ec7262f0d0df6e07d32b409509c11a0eb5
Raik176 <raikmaus321@gmail.com>
2024-03-23 11:46:28 +0100

    [fix] Project build action

---
+++ SMW-Rewrite/Scripts/MarioActor.cs
@@ -0,0 +1,18 @@
+﻿using Raylib_cs;
+using SMW_Rewrite.Scripts.UI;
+
+namespace SMW_Rewrite.Scripts {
+    internal class MarioActor : UIElement {
+        public MarioActor(Rectangle bounds) : base(bounds) {
+
+        }
+
+        public override void Destroy() {
+            
+        }
+
+        public override void Render() {
+            
+        }
+    }
+}
